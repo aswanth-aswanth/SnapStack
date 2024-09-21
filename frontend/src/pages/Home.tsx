e0a553977fe6e54be83e101feaa368f4060fe930
aswanth-aswanth <aswanthndl@gmail.com>
2024-09-21 09:15:36 +0530

    feat: Improve home page design

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import Parent from "../components/ImageGallery/Parent";
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
+    <div className="flex flex-col items-center justify-center min-h-[70vh]">
       <h1 className="text-3xl font-bold mb-4">
         Welcome to the Image Management App
       </h1>
